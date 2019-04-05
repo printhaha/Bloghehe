699407838f38a5c84526d56beb46600a557e5913
printhaha <942295096@qq.com>
2019-04-05 13:56:05 +0800

    更新了登录退出方法

--- src/main/java/com/itshidu/web/controller/LoginController.java
+++ src/main/java/com/itshidu/web/controller/LoginController.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.Map;
 
 @Controller
@@ -18,5 +19,10 @@ public class LoginController {
     @RequestMapping("/public/login")
     public Result Login(String username, String password){
        return _LoginService.Login(username,password);
+    }
+    @RequestMapping("/public/logout")
+    public Object Logout(HttpServletRequest request){
+        request.getSession().removeAttribute("loginInfo");
+        return "redirect:/login.html";
     }
 }
